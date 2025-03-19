5b5bf9ba21047c7d8264ba4957bab447d9b1c0ce
Gabriella <gabriella@u.nus.edu>
2025-03-19 21:21:21 +0800

    Change Person card view

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -58,6 +58,7 @@ public class PersonCard extends UiPart<Region> {
         email.setText(person.getEmail().value);
         jobPosition.setText(person.getJobPosition().jobPosition);
         status.setText(person.getStatus().value);
+        
         person.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
